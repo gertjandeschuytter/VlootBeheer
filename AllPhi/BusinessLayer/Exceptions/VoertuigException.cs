2b9ac4af6fd0750a2ae08067e26f90f56a0d02f5
Gertjan Deschuytter <gertjan.deschuytter@student.hogent.be>
2021-10-07 15:30:45 +0200

    constructors aangepast

---
+++ AllPhi/BusinessLayer/Exceptions/VoertuigException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Exceptions {
+    class VoertuigException : Exception {
+
+        public VoertuigException(string message) : base(message) {
+        }
+
+        public VoertuigException(string message, Exception innerException) : base(message, innerException) {
+        }
+    }
+}
--- AllPhi/BusinessLayer/Model/Voertuig.cs
+++ AllPhi/BusinessLayer/Model/Voertuig.cs
@@ -5,25 +5,62 @@ namespace BusinessLayer.Model
 {
     public class Voertuig
     {
-        //properties
-        public string Merk { get; private set; }
-        public string Model { get; private set; }
+        #region Properties
+        public string MerkEnModel { get; private set; }
         public string ChassisNummer { get; private set; }
         public string NummerPlaat { get; private set; }
         public Brandstoftype BrandstofType { get; private set; }
         public Typewagen TypeWagen { get; private set; }
-
-        //fields
-        private List<string> Merken = new List<string>();
-
-        #region Constructors
-        public Voertuig()
-        {
-
-        }
+        public string Kleur { get; private set; }
+        public int AantalDeuren { get; private set; }
+        public Bestuurder Bestuurder { get; private set; }
         #endregion
 
-        #region Properties
+        #region fields
+        private readonly List<string> Merken = new List<string>();
+        #endregion
+
+        #region Constructors
+
+        public Voertuig(string merkEnModel, string chassisNummer, string nummerPlaat, Brandstoftype brandstofType, Typewagen typeWagen, string kleur, int aantalDeuren, Bestuurder bestuurder) {
+            MerkEnModel = merkEnModel;
+            ChassisNummer = chassisNummer;
+            NummerPlaat = nummerPlaat;
+            BrandstofType = brandstofType;
+            TypeWagen = typeWagen;
+            Kleur = kleur;
+            AantalDeuren = aantalDeuren;
+            Bestuurder = bestuurder;
+        }
+
+        public Voertuig(string merkEnModel, string chassisNummer, string nummerPlaat, Brandstoftype brandstofType, Typewagen typeWagen) {
+            MerkEnModel = merkEnModel;
+            ChassisNummer = chassisNummer;
+            NummerPlaat = nummerPlaat;
+            BrandstofType = brandstofType;
+            TypeWagen = typeWagen;
+        }
+
+        public Voertuig(string merkEnModel, string chassisNummer, string nummerPlaat, Brandstoftype brandstofType, Typewagen typeWagen, string kleur) : this(merkEnModel, chassisNummer, nummerPlaat, brandstofType, typeWagen) {
+            Kleur = kleur;
+        }
+
+        public Voertuig(string merkEnModel, string chassisNummer, string nummerPlaat, Brandstoftype brandstofType, Typewagen typeWagen, string kleur, int aantalDeuren) : this(merkEnModel, chassisNummer, nummerPlaat, brandstofType, typeWagen, kleur) {
+            AantalDeuren = aantalDeuren;
+        }
+
+        public Voertuig(string merkEnModel, string chassisNummer, string nummerPlaat, Brandstoftype brandstofType, Typewagen typeWagen, string kleur, Bestuurder bestuurder) : this(merkEnModel, chassisNummer, nummerPlaat, brandstofType, typeWagen, kleur) {
+            Bestuurder = bestuurder;
+        }
+
+        public Voertuig(string merkEnModel, string chassisNummer, string nummerPlaat, Brandstoftype brandstofType, Typewagen typeWagen, int aantalDeuren, Bestuurder bestuurder) : this(merkEnModel, chassisNummer, nummerPlaat, brandstofType, typeWagen) {
+            AantalDeuren = aantalDeuren;
+            Bestuurder = bestuurder;
+        }
+
+
+
+
         #endregion
 
         #region Methods
