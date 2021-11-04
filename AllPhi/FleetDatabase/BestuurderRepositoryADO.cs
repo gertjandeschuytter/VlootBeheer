6498e7ae325f5c5d2dce903dff250804c1064ad1
JarneOph <jarne.ophalvens@student.hogent.be>
2021-11-04 13:15:21 +0100

    26 changes?

---
+++ AllPhi/FleetDatabase/BestuurderRepositoryADO.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLayer.Interfaces;
+using BusinessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetDatabase
+{
+    public class BestuurderRepositoryADO : IBestuurderRepository
+    {
+        public Bestuurder GeefBestuurder(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Bestuurder> GeefBestuurders(string naam, string voornaam, Func<string> toString, DateTime datum, string rijksregister, List<TypeRijbewijs> types, Voertuig v, TankKaart t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HeeftBestuurder(Bestuurder bestuurder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VerwijderBestuurder(Bestuurder bestuurder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VoegBestuurderToe(Bestuurder bestuurder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WijzigBestuurder(Bestuurder bestuurder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
