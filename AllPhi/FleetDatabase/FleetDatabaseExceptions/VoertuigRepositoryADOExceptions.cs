16587d93878a1370fc16612303d49186dab7a718
gertjands <gertjan.deschuytter@student.hogent.be>
2021-11-18 13:39:51 +0100

    .

---
+++ AllPhi/FleetDatabase/FleetDatabaseExceptions/VoertuigRepositoryADOExceptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetDatabase.FleetDatabaseExceptions {
+    public class VoertuigRepositoryADOExceptions : Exception {
+        public VoertuigRepositoryADOExceptions() {
+        }
+
+        public VoertuigRepositoryADOExceptions(string message) : base(message) {
+        }
+
+        public VoertuigRepositoryADOExceptions(string message, Exception innerException) : base(message, innerException) {
+        }
+
+        protected VoertuigRepositoryADOExceptions(SerializationInfo info, StreamingContext context) : base(info, context) {
+        }
+    }
+}
