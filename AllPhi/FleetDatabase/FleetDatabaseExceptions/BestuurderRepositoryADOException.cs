2130fc0cd96437235eea30533c1d27a628bba007
JarneOph <jarne.ophalvens@student.hogent.be>
2021-11-24 16:13:02 +0100

    Model + UnitTesten aangepast

---
+++ AllPhi/FleetDatabase/FleetDatabaseExceptions/BestuurderRepositoryADOException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetDatabase.FleetDatabaseExceptions
+{
+    class BestuurderRepositoryADOException : Exception
+    {
+        public BestuurderRepositoryADOException(string message) : base(message)
+        {
+        }
+
+        public BestuurderRepositoryADOException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
--- AllPhi/UnitTests/VoertuigTests.cs
+++ AllPhi/UnitTests/VoertuigTests.cs
@@ -2,31 +2,32 @@
 using BusinessLayer.Model;
 using BusinessLayer.Exceptions;
 using Xunit;
+using System.Collections.Generic;
 
 namespace UnitTests {
     public class VoertuigTests {
         //eigen tests
         [Fact]
         public void Test_CtorEssential_Valid() {
-            Voertuig v = new("peugot", "308", "A15KSLSK124ND523A", "1ABC325", Brandstoftype.Benzine, Typewagen.personenwagen);
+            Voertuig v = new("peugot", "308", "A15KSLSK124ND523A", "1ABC325", Brandstoftype_voertuig.Benzine, Typewagen.personenwagen);
             Assert.Equal("peugot", v.Merk);
             Assert.Equal("308", v.Model);
             Assert.Equal("A15KSLSK124ND523A", v.ChassisNummer);
             Assert.Equal("1ABC325", v.NummerPlaat);
-            Assert.Equal(Brandstoftype.Benzine, v.BrandstofType);
+            Assert.Equal(Brandstoftype_voertuig.Benzine, v.BrandstofType);
             Assert.Equal(Typewagen.personenwagen, v.TypeWagen);
         }
         [Theory]
         [InlineData(" ")]
         [InlineData(null)]
         public void Test_CtorEssential_MerkInValid(string merk) {
-            Assert.Throws<VoertuigException>(() => new Voertuig(merk, "308", "A15KSLSK124ND523", "1ABC3253", Brandstoftype.Benzine, Typewagen.personenwagen));
+            Assert.Throws<VoertuigException>(() => new Voertuig(merk, "308", "A15KSLSK124ND523", "1ABC3253", Brandstoftype_voertuig.Benzine, Typewagen.personenwagen));
         }
         [Theory]
         [InlineData(" ")]
         [InlineData(null)]
         public void Test_CtorEssential_ModelInvalid(string model) {
-            Assert.Throws<VoertuigException>(() => new Voertuig("peugot", model, "A15KSLSK124ND523", "1ABC3253", Brandstoftype.Benzine, Typewagen.personenwagen));
+            Assert.Throws<VoertuigException>(() => new Voertuig("peugot", model, "A15KSLSK124ND523", "1ABC3253", Brandstoftype_voertuig.Benzine, Typewagen.personenwagen));
         }
         [Theory]
         [InlineData(" ")]
@@ -35,7 +36,7 @@ namespace UnitTests {
         [InlineData("A15KSLSK123ND52")]
         [InlineData(null)]
         public void Test_CtorEssential_ChassisnummerInValid(string chassisNumer) {
-            Assert.Throws<VoertuigException>(() => new Voertuig("peugot", "308", chassisNumer, "1ABC3253", Brandstoftype.Benzine, Typewagen.personenwagen));
+            Assert.Throws<VoertuigException>(() => new Voertuig("peugot", "308", chassisNumer, "1ABC3253", Brandstoftype_voertuig.Benzine, Typewagen.personenwagen));
         }
         [Theory]
         [InlineData(" ")]
@@ -46,16 +47,18 @@ namespace UnitTests {
         [InlineData("AABC123")]
         [InlineData(null)]
         public void Test_CtorEssential_nummerplaatInValid(string nummerplaat) {
-            Assert.Throws<VoertuigException>(() => new Voertuig("peugot", "308", "A15KSLSK124ND523", nummerplaat, Brandstoftype.Benzine, Typewagen.personenwagen));
+            Assert.Throws<VoertuigException>(() => new Voertuig("peugot", "308", "A15KSLSK124ND523", nummerplaat, Brandstoftype_voertuig.Benzine, Typewagen.personenwagen));
         }
 
         [Fact]
         public void Test_Ctor_All_Valid() {
+            List<TypeRijbewijs> types = new List<TypeRijbewijs>();
+            types.Add(TypeRijbewijs.C);
             Adres adres = new("DiepenBroekStraat", "Balegem", "9860", 1);
             TankKaart tankkaart = new("0123456789", new DateTime(2022, 08, 01));
-            Bestuurder bestuurder = new("Ophalvens", "Jarne", adres, new DateTime(1999, 08, 04), "99080455307", tankkaart);
+            Bestuurder bestuurder = new("Ophalvens", "Jarne", adres, new DateTime(1999, 08, 04), "99080455307", tankkaart, types);
 
-            Voertuig voertuig = new("Toyota", "Fiesta", "01234567891234567", "1ABC123", Brandstoftype.Benzine, Typewagen.personenwagen,bestuurder);
+            Voertuig voertuig = new("Toyota", "Fiesta", "01234567891234567", "1ABC123", Brandstoftype_voertuig.Benzine, Typewagen.personenwagen,bestuurder);
             Assert.Equal("Toyota", voertuig.Merk);
             Assert.Equal("Fiesta", voertuig.Model);
             Assert.Equal("01234567891234567", voertuig.ChassisNummer);
@@ -70,11 +73,13 @@ namespace UnitTests {
         [InlineData(" ")]
         [InlineData(null)]
         public void Test_Ctor_All_InvalidMerk(string merk) {
+            List<TypeRijbewijs> types = new List<TypeRijbewijs>();
+            types.Add(TypeRijbewijs.C);
             Adres adres = new("DiepenBroekStraat", "Balegem", "9860", 1);
             TankKaart tankkaart = new("0123456789", new DateTime(2022, 08, 01));
-            Bestuurder bestuurder = new("Ophalvens", "Jarne", adres, new DateTime(1999, 08, 04), "99080455307", tankkaart);
+            Bestuurder bestuurder = new("Ophalvens", "Jarne", adres, new DateTime(1999, 08, 04), "99080455307", tankkaart, types);
 
-            Assert.Throws<VoertuigException>(() => new Voertuig(merk, "Fiesta", "01234567891234567", "1ABC123", Brandstoftype.Benzine, Typewagen.personenwagen, bestuurder));
+            Assert.Throws<VoertuigException>(() => new Voertuig(merk, "Fiesta", "01234567891234567", "1ABC123", Brandstoftype_voertuig.Benzine, Typewagen.personenwagen, bestuurder));
         }
 
         [Theory]
@@ -82,29 +87,35 @@ namespace UnitTests {
         [InlineData(" ")]
         [InlineData(null)]
         public void Test_Ctor_All_InvalidModel(string model) {
+            List<TypeRijbewijs> types = new List<TypeRijbewijs>();
+            types.Add(TypeRijbewijs.C);
             Adres adres = new("DiepenBroekStraat", "Balegem", "9860", 1);
             TankKaart tankkaart = new("0123456789", new DateTime(2022, 08, 01));
-            Bestuurder bestuurder = new("Ophalvens", "Jarne", adres, new DateTime(1999, 08, 04), "99080455307", tankkaart);
+            Bestuurder bestuurder = new("Ophalvens", "Jarne", adres, new DateTime(1999, 08, 04), "99080455307", tankkaart, types);
 
-            Assert.Throws<VoertuigException>(() => new Voertuig("Toyota", model, "01234567891234567", "1ABC123", Brandstoftype.Benzine, Typewagen.personenwagen, bestuurder));
+            Assert.Throws<VoertuigException>(() => new Voertuig("Toyota", model, "01234567891234567", "1ABC123", Brandstoftype_voertuig.Benzine, Typewagen.personenwagen, bestuurder));
         }
 
         [Fact]
         public void Test_Ctor_All_InvalidChassisnummer() {
+            List<TypeRijbewijs> types = new List<TypeRijbewijs>();
+            types.Add(TypeRijbewijs.C);
             Adres adres = new("DiepenBroekStraat", "Balegem", "9860", 1);
             TankKaart tankkaart = new("0123456789", new DateTime(2022, 08, 01));
-            Bestuurder bestuurder = new("Ophalvens", "Jarne", adres, new DateTime(1999, 08, 04), "99080455307", tankkaart);
+            Bestuurder bestuurder = new("Ophalvens", "Jarne", adres, new DateTime(1999, 08, 04), "99080455307", tankkaart, types);
 
-            Assert.Throws<VoertuigException>(() => new Voertuig("Toyota", "Fiesta", null, "1ABC123", Brandstoftype.Benzine, Typewagen.personenwagen, bestuurder));
+            Assert.Throws<VoertuigException>(() => new Voertuig("Toyota", "Fiesta", null, "1ABC123", Brandstoftype_voertuig.Benzine, Typewagen.personenwagen, bestuurder));
         }
 
         [Fact]
         public void Test_Ctor_All_InvalidNummerplaat() {
+            List<TypeRijbewijs> types = new List<TypeRijbewijs>();
+            types.Add(TypeRijbewijs.C);
             Adres adres = new("DiepenBroekStraat", "Balegem", "9860", 1);
             TankKaart tankkaart = new("0123456789", new DateTime(2022, 08, 01));
-            Bestuurder bestuurder = new("Ophalvens", "Jarne", adres, new DateTime(1999, 08, 04), "99080455307", tankkaart);
+            Bestuurder bestuurder = new("Ophalvens", "Jarne", adres, new DateTime(1999, 08, 04), "99080455307", tankkaart, types);
 
-            Assert.Throws<VoertuigException>(() => new Voertuig("Toyota", "Fiesta", "01234567891234567", "5ABC123", Brandstoftype.Benzine, Typewagen.personenwagen, bestuurder));
+            Assert.Throws<VoertuigException>(() => new Voertuig("Toyota", "Fiesta", "01234567891234567", "5ABC123", Brandstoftype_voertuig.Benzine, Typewagen.personenwagen, bestuurder));
         }
     }
 }
