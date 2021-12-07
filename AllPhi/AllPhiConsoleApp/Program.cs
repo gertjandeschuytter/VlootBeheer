7a3e50df84f74f550b58646b8064e265a46cc0d0
gertjands <gertjan.deschuytter@student.hogent.be>
2021-12-07 18:21:53 +0100

    ADOLayer afgewerkt behalve tankkaart

--- AllPhi/AllPhiConsoleApp/Program.cs
+++ AllPhi/AllPhiConsoleApp/Program.cs
@@ -12,17 +12,27 @@ namespace AllPhiConsoleApp {
             //b.GeefBestuurder(4);
             //b.BestaatBestuurder(2);
             //Bestuurder bestuurder = new Bestuurder("Pol", "Segers", new DateTime(1999,08,04), "99080455307", list);
-
-            Bestuurder bestuurder = b.GeefBestuurder(4);
-            bestuurder.ZetNaam("Hannes");
-            b.WijzigBestuurder(bestuurder);
+            //Bestuurder bestuurder = b.GeefBestuurder(4);
+            //bestuurder.Voertuig.ZetModel("SportBack f6");
+            //bestuurder.TankKaart.ZetPincode("4321");
+            //b.WijzigBestuurder(bestuurder);
+            //Bestuurder bestuurder = b.GeefBestuurder(4);
+            //b.GeefBestuurders("Gertjan", null, null, null);
+            //b.VerwijderBestuurder(bestuurder);
             //b.VoegBestuurderToe(bestuurder);
-
             //bestuurder.VoegRijbewijsToe(TypeRijbewijs.C);
             //Voertuig v = new Voertuig("Toyota", "393", "12345678911234567", "1ABC123", Brandstoftype_voertuig.Diesel, Typewagen.personenwagen);
             //bestuurder.ZetVoertuig(v);
-
             //b.WijzigBestuurder(bestuurder);
+            VoertuigRepositoryADO v = new VoertuigRepositoryADO(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Fleet;Integrated Security=True");
+            //v.GeefVoertuigen("Toyota", "Yaris", "Elektrisch","Personenwagen","Rood",5);
+            //Voertuig voertuig1 = v.geefVoertuig(1);
+            //voertuig1.ZetAantalDeuren(8);
+            //v.UpdateVoertuig(voertuig1);
+            //Voertuig voertuig = v.geefVoertuig(1);
+            //voertuig.ZetKleur("Paars");
+            //voertuig.ZetModel("Bandito");
+            //v.VoegVoertuigToe(voertuig);
         }
     }
 }
---
+++ AllPhi/BusinessLayer/Utilities/CompareSerializer.cs
@@ -0,0 +1,13 @@
+﻿using BusinessLayer.Model;
+using Castle.Core;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Utilities {
+    public class CompareSerializer {
+    }
+}
