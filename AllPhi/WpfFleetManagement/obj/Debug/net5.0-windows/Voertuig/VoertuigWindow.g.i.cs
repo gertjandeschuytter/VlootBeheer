dea9ca194ad9faddc65c46785638a9496fc584bc
JarneOph <jarne.ophalvens@student.hogent.be>
2022-01-12 13:58:12 +0100

    commit before pull

--- AllPhi/WpfFleetManagement/obj/Debug/net5.0-windows/Voertuig/VoertuigWindow.g.i.cs
+++ AllPhi/WpfFleetManagement/obj/Debug/net5.0-windows/Voertuig/VoertuigWindow.g.i.cs
@@ -245,7 +245,7 @@ namespace WpfFleetManagement.Voertuig {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfFleetManagement;component/voertuig/voertuigwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfFleetManagement;V1.0.0.0;component/voertuig/voertuigwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Voertuig\VoertuigWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
